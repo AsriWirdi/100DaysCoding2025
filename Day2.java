a850938969cdb0b827ee420c6e8baa09f237d337
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-09-09 17:36:25 +0800

    Add files via upload
    100 Days Coding 2025
    Day 2: Penggunaan println(), print(), dan printf()

---
+++ Day2.java
@@ -0,0 +1,7 @@
+public class Day2 {
+  public static void main(String... args) {
+    System.out.println("Haloo sobat...");
+    System.out.print("Perkenalkan, saya ");
+    System.out.printf("%s", "Asri Wirdi");
+  }
+}
