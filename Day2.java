d4acd1d822a6acc6398371e3904624ab0cb27906
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-09-10 15:34:36 +0800

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -1,7 +1,20 @@
 public class Day2 {
   public static void main(String... args) {
-    System.out.println("Haloo sobat...");
-    System.out.print("Perkenalkan, saya ");
-    System.out.printf("%s", "Asri Wirdi");
+    String nama = "Asri Wirdi";
+    int umur = 19;
+    double tinggiBadan = 165.50;
+
+    // println
+    System.out.println("Nama =  " + nama);
+    System.out.println("Umur = " + umur);
+    System.out.println("Tinggi Badan: " + tinggiBadan + "\n");
+    
+    // print
+    System.out.print("Nama =  " + nama);
+    System.out.print("Umur = " + umur);
+    System.out.print("Tinggi Badan: " + tinggiBadan + "\n\n");
+
+    // printf
+    System.out.printf("Nama: %s%nUmur: %d%nTinggi Badan: %.2f", nama, umur, tinggiBadan);
   }
 }
