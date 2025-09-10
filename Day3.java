999755d71e5a004548fd1db9be0b3b34ca7a38f3
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-09-10 15:53:13 +0800

    Create Day3.java
    100 Days Of Coding 2025
    Day 3: Memahami dan menggunakan Komentar

---
+++ Day3.java
@@ -0,0 +1,17 @@
+public class Day3 {
+  public static void main(String... args) {
+    // Memahami dan menggunakan Komentar
+    /* komentar pada Java adalah teks yang ditulis dalam kode tapi tidak dijalankan oleh compiler
+       Jadi, fungsi komentar itu untuk memberikan penjelasan pada kode agar mudah dipahami,
+       bisa juga untuk menonaktifkan sementara baris kode saat debuging.
+       Dijava ada 2 jenis komentar yaitu, komentar satu baris, dan komentar multi baris(lebih dari satu baris).
+    */
+
+    // System.out.println("komentar satu baris");
+
+    /*
+    String coment = "ini adalah komentar multi baris";
+    System.out.println(coment);
+    */
+  }
+}
