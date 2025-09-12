63c8a45de1178dbc3b394aa4186325f24a1d6ae0
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-09-12 18:37:22 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 5: Tipe Data Numerik Bilangan Bulat (byte, short, int, long).

---
+++ Day5.java
@@ -0,0 +1,18 @@
+public class Day5 {
+  public static void main(String... args) {
+    /*
+     * 100 Days Of Coding 2025
+     * Day 5: Tipe Data Numerik Bilangan Bulat (byte, short, int, long).
+     */
+
+    byte umur = 19;
+    short tahun = 2025;
+    int limaRibu = 5000;
+    long populasi = 7800000000L;
+
+    System.out.println(umur);
+    System.out.println(tahun);
+    System.out.println(limaRibu);
+    System.out.println(populasi);
+  }
+}
