699872a38060ea221384b456f4f8d75365d42757
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-09-13 12:01:20 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 6: Tipe Data Pecahan (double, float).

---
+++ Day6.java
@@ -0,0 +1,14 @@
+public class Day6 {
+  public static void main(String... args) {
+    /*
+     * 100 Days Of Coding 2025
+     * Day 6: Tipe Data Pecahan (double, float).
+     */
+
+    float tinggiBadan = 165.5f;
+    double beratBadan = 50.5;
+
+    System.out.println(tinggiBadan);
+    System.out.println(beratBadan);
+  }
+}
