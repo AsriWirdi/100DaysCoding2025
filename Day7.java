4cdd53850054c1182911b52d5ddace1d594f3320
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-09-14 15:57:17 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 7: Tipe Data Karakter (char) dan Boolean (boolean).

---
+++ Day7.java
@@ -0,0 +1,28 @@
+public class Day7 {
+  public static void main(String... args) {
+    /*
+     * 100 Days Of Coding 2025
+     * Day 7: Tipe Data Karakter (char) dan Boolean (boolean).
+     */
+
+    char char1 = 'A';
+    char char2 = 's';
+    char char3 = 'r';
+    char char4 = 'i';
+    char char5 = ' ';
+    char char6 = 'W';
+    char char7 = 'i';
+    char char8 = 'r';
+    char char9 = 'd';
+    char char10 = 'i';
+    boolean isPria = true; // boolean values: true or false.
+
+    System.out.printf("%c%c%c%c%c%c%c%c%c%c%n", char1, char2, char3, char4, char5, char6, char7, char8, char9, char10);
+
+    System.out.println("Apakah Pria? " + isPria);
+
+    // OutPut:
+    // Asri Wirdi
+    // Apakah Pria? true
+  }
+}
