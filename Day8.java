40f4308a92a4d6718d0f40606b4dc11794d9a6d6
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-09-15 18:49:38 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 8: Pengenalan Tipe Data String untuk teks.

---
+++ Day8.java
@@ -0,0 +1,16 @@
+public class Day8 {
+  public static void main(String... args) {
+    /*
+     * 100 Days Of Coding 2025
+     * Day 8: Pengenalan Tipe Data String untuk teks.
+     */
+
+    String intro = "Haloo...\nPerkenalkan saya Asri Wirdi. Saya berasal dari Kabupaten Pasangkayu";
+
+    System.out.println(intro);
+
+    // OutPut:
+    // Haloo...
+    // Perkenalkan saya Asri Wirdi. Saya berasal dari Kabupaten Pasangkayu
+  }
+}
