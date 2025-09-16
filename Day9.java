2d68a807f1376388e40d2a1abc2420b75269e3c8
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-09-16 18:31:24 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 9: Deklarasi, Inisialisasi dan Update Variabel.

---
+++ Day9.java
@@ -0,0 +1,17 @@
+public class Day9 {
+  public static void main(String... args) {
+    /*
+     * 100 Days Of Coding 2025
+     * Day 9: Deklarasi, Inisialisasi dan Update Variabel.
+     */
+
+     // deklarasi variable angka dengan tipe data int.
+    int angka;
+
+    // Inisialisasi variable.
+    angka = 2005;
+
+    // Update variable
+    angka = 2025;    
+  }
+}
