12631d1e353f121b8e690a05c2a09ae91b436929
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-09-17 22:35:33 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 10: Constanta (Final Variabel).

---
+++ Day10.java
@@ -0,0 +1,11 @@
+public class Day10 {
+  public static void main(String... args) {
+    /*
+     * 100 Days Of Coding 2025
+     * Day 10: Constanta (Final Variabel).
+     */
+
+    final int kelahiran = 2005;
+    // kelahiran = 2006; error karena variable final tidak bisa di update.
+  }
+}
