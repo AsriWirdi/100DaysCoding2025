33c445cea9835b9e50d81e0adf19db751f0c4901
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-09-22 18:04:58 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 15: Operator Aritmatika Penjumlahan (+) dan Pengurangan (-).

---
+++ Day15.java
@@ -0,0 +1,28 @@
+public class Day15 {
+  public static void main(String[] args) {
+
+    /*
+     * 100 Days Of Coding 2025
+     * Day 15: Operator Aritmatika Penjumlahan (+) dan Pengurangan (-).
+     */
+
+    int a = 10;
+    int b = 5;
+
+    double c = 5.5;
+    double d = 1.2;
+
+    // Operator penjumlahan
+    int tambah = a + b;
+    
+    // Operator penjumlahan
+    double kurang = c - d;
+
+    System.out.println(tambah);
+    System.out.println(kurang);
+
+
+    
+  }
+
+}
