ae46a20827904a016b21525cf129c58f3c13e1e0
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-09-23 22:13:08 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 16: Operator Aritmatika Perkalian (*) dan Pembagian (/).

---
+++ Day16.java
@@ -0,0 +1,22 @@
+public class Day16 {
+  public static void main(String[] args) {
+
+    /*
+     * 100 Days Of Coding 2025
+     * Day 16: Operator Aritmatika Perkalian (*) dan Pembagian (/).
+     */
+
+    int a = 10;
+    int b = 5;
+
+    // Operator Perkalian
+    int perkalian = a * b;
+
+    // Operator Pembagian
+    int pembagian = a / b;
+
+    System.out.println(perkalian);
+    System.out.println(pembagian);
+  }
+
+}
