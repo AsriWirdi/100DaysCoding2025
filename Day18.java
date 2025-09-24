9203419049dd29d61bb9e522550703daa2bec73d
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-09-25 01:49:37 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 18: Konversi Otomatis (Dari Tipe Data Kecil ke Besar).

---
+++ Day18.java
@@ -0,0 +1,17 @@
+public class Day18 {
+  public static void main(String[] args) {
+
+    /*
+     * 100 Days Of Coding 2025
+     * Day 18: Konversi Otomatis (Dari Tipe Data Kecil ke Besar).
+     */
+
+    byte a = 100;
+    short b = a;
+    int c = b;
+    long d = c;
+    double e = d;
+
+    System.out.println(d);
+  }
+}
