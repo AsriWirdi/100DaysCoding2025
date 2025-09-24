7430a250390bd22d6d1143bcc4373b5dd8f56e9a
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-09-24 22:56:03 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 17: Operator Aritmatika Modulus atau Sisa Bagi (%).

---
+++ Day17.java
@@ -0,0 +1,17 @@
+public class Day17 {
+  public static void main(String[] args) {
+
+    /*
+     * /*
+     * 100 Days Of Coding 2025
+     * Day 17: Operator Aritmatika Modulus atau Sisa Bagi (%).
+     */
+
+    int a = 10;
+
+    int hasilMod1 = a % 3;
+    int hasilMod2 = a % 5;
+    System.out.println(hasilMod1);
+    System.out.println(hasilMod2);
+  }
+}
