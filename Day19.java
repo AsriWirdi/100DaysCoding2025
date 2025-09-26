1f9562c64dbaa70cacce2bdc8b168fbbf49b622e
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-09-26 19:04:34 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 19: Konversi Manual/Paksa (Dari Tipe Data Besar ke Kecil).

---
+++ Day19.java
@@ -0,0 +1,16 @@
+public class Day19 {
+  public static void main(String[] args) {
+
+    /*
+     * 100 Days Of Coding 2025
+     * Day 19: Konversi Manual/Paksa (Dari Tipe Data Besar ke Kecil).
+     */
+
+    long a = 100;
+    int b = (int) a;
+    short c = (short) b;
+    byte d = (byte) c;
+
+    System.out.println(d);
+  }
+}
