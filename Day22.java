dc5160428267903569d4f8e6b06771dd89253bd7
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-09-29 00:37:41 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 22: Latihan: Program Luas Persegi.

---
+++ Day22.java
@@ -0,0 +1,16 @@
+public class Day22 {
+  public static void main(String[] args) {
+
+    /*
+     * 100 Days Of Coding 2025
+     * Day 22: Latihan: Program Luas Persegi.
+     */
+
+    int sisi = 4;
+    int luas = sisi * sisi;
+
+    System.out.println("Luas Persegi = sisi x sisi");
+    System.out.printf("Luas Persegi = %d x %d\n", sisi, sisi);
+    System.out.printf("Luas Persegi = %d", luas);
+  }
+}
