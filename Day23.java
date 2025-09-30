1f23f647285dd53daef9488c4d70d66729c41ed5
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-09-30 20:13:43 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 23: Latihan: Program Luas Persegi Panjang.

---
+++ Day23.java
@@ -0,0 +1,15 @@
+public class Day23 {
+  public static void main(String[] args) {
+
+    /*
+     * 100 Days Of Coding 2025
+     * Day 23: Latihan: Program Luas Persegi Panjang.
+     */
+
+    int panjang = 4;
+    int lebar = 2;
+    int luas = panjang * lebar;
+
+    System.out.println("luas Persegi Panjang = " + luas);
+  }
+}
