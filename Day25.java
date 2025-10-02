a661ba9968f8ed45870c679e628d894796b9cd47
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-10-02 19:41:29 +0800

    Add files via upload
    100 Days Of Coding 2025
     Day 25: Operator Increment dan Decrement (++, --)

---
+++ Day25.java
@@ -0,0 +1,19 @@
+public class Day25 {
+  public static void main(String[] args) {
+
+    /*
+     * 100 Days Of Coding 2025
+     * Day 25: Operator Increment dan Decrement (++, --)
+     */
+
+    int i = 0;
+
+    // increment
+    i++;
+    System.out.println(i);
+
+    // Decrement
+    i--;
+    System.out.println(i);
+  }
+}
