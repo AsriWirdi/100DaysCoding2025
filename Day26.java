26a38fb0b1775ce87b824df4cc19b76eeda39eb8
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-10-03 20:42:49 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 26: Operator Penugasan (=, +=, -=, dll).

---
+++ Day26.java
@@ -0,0 +1,17 @@
+public class Day26 {
+  public static void main(String[] args) {
+
+    /*
+     * 100 Days Of Coding 2025
+     * Day 26: Operator Penugasan (=, +=, -=, dll).
+     */
+
+    int a = 2;
+
+    a += 5; // sama saja dengan a = a + 5;
+    a -= 3; // sama saja dengan a = a - 3;
+    a *= 2; // sama saja dengan a = a * 2;
+    a /= 2; // sama saja dengan a = a / 2;
+    a %= 2; // sama saja dengan a = a % 2;
+  }
+}
