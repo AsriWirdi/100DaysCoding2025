1cbf228e0830fdb95052efe185ccc8800a7cc0c5
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-10-05 16:20:11 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 28: Operator Perbandingan == dan !=

---
+++ Day28.java
@@ -0,0 +1,14 @@
+public class Day28 {
+  public static void main(String[] args) {
+
+    /*
+     * 100 Days Of Coding 2025
+     * Day 28: Operator Perbandingan == dan !=
+     */
+
+    int x = 0;
+    int y = 1;
+    System.out.println(x == y); // false
+    System.out.println(x != y); // true
+  }
+}
