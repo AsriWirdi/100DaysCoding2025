2f5a5148e93afd93789e907529dba07f31b08331
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-10-07 08:29:08 +0800

    Add files via upload
    100 Days Of Coding 2025
     Day 30: Operator Perbandingan <= dan >=.

---
+++ Day30.java
@@ -0,0 +1,17 @@
+public class Day30 {
+  public static void main(String[] args) {
+
+    /*
+     * 100 Days Of Coding 2025
+     * Day 30: Operator Perbandingan <= dan >=.
+     */
+
+    int x = 0;
+    int y = 1;
+    int z = 0;
+    System.out.println(x <= y); // true
+    System.out.println(x <= z); // true
+    System.out.println(x >= y); // false
+    System.out.println(x >= z); // true
+  }
+}
