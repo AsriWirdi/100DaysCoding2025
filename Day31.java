a21859dee8a3e6692a783e9270f73234c621267f
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-10-08 22:09:58 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 31: Operator Logika AND (&&).

---
+++ Day31.java
@@ -0,0 +1,18 @@
+public class Day31 {
+  public static void main(String[] args) {
+
+    /*
+     * 100 Days Of Coding 2025
+     * Day 31: Operator Logika AND (&&).
+     */
+
+    int x = 0;
+    int y = 1;
+    int z = 2;
+
+    System.out.println("bilangan paling besar di antara ke 3 variable: ");
+    System.out.printf("x adalah paling besar, %b%n",(x > y) && (x > y)); // false
+    System.out.printf("y adalah paling besar, %b%n",(y > x) && (y > z)); // false
+    System.out.printf("z adalah paling besar, %b%n",(z > x) && (z > y)); // true
+  }
+}
