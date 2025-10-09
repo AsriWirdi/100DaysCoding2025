781a6e9eaadffec7e59b80d4f8b42228264b1aa7
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-10-10 00:09:28 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 33: Operator Logika NOT (!).

---
+++ Day33.java
@@ -0,0 +1,16 @@
+
+public class Day33 {
+  public static void main(String[] args) {
+
+    /*
+     * 100 Days Of Coding 2025
+     * Day 33: Operator Logika NOT (!).
+     */
+
+    boolean benar = true;
+    boolean salah = false;
+
+    System.out.println("hari kebalikan, benar itu " + !benar + " salah itu " + !salah);
+  }
+
+}
