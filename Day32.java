574b8edd2046904734eca78a955fa81bc1c54359
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-10-09 22:20:43 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 32: Operator Logika OR (||).

---
+++ Day32.java
@@ -0,0 +1,20 @@
+
+public class Day32 {
+  public static void main(String[] args) {
+
+    /*
+     * 100 Days Of Coding 2025
+     * Day 32: Operator Logika OR (||).
+     */
+
+    int umur = 25;
+    int tahunPengalamanKerja = 4;
+    boolean syarat = (umur >= 20 || tahunPengalamanKerja > 5);
+
+    System.out.println("Syarat masuk Perusahaan A, setidaknya minimal umur 20 tahun atau Pengalaman kerja minimal 5 tahun ");
+    System.out.println("umur: " + umur);
+    System.out.println("Pengalaman Kerja: " + tahunPengalamanKerja + " Tahun");
+    System.out.println("Diterima: " + syarat);
+  }
+
+}
