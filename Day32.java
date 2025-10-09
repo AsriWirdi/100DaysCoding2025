96372baac9a33157104e2d00b2b3d2d506265e90
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-10-09 22:33:29 +0800

    Update Day32.java

--- Day32.java
+++ Day32.java
@@ -9,12 +9,12 @@ public class Day32 {
 
     int umur = 25;
     int tahunPengalamanKerja = 4;
-    boolean syarat = (umur >= 20 || tahunPengalamanKerja > 5);
+    boolean syarat = (umur >= 20 || tahunPengalamanKerja >= 5);
 
-    System.out.println("Syarat masuk Perusahaan A, setidaknya minimal umur 20 tahun atau Pengalaman kerja minimal 5 tahun ");
+    System.out
+        .println("Syarat masuk Perusahaan A, setidaknya minimal umur 20 tahun atau Pengalaman kerja minimal 5 tahun ");
     System.out.println("umur: " + umur);
     System.out.println("Pengalaman Kerja: " + tahunPengalamanKerja + " Tahun");
     System.out.println("Diterima: " + syarat);
   }
-
 }
