e7cda13bca92fab6a07d3b58a4e22dc185e7f642
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-10-12 22:17:22 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 35: Percabangan (if)

---
+++ Day35.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Day35 {
+  public static void main(String[] args) {
+
+    /*
+     * 100 Days Of Coding 2025
+     * Day 35: Percabangan (if)
+     */
+
+    int angka = 11;
+    boolean ganjil = angka % 2 == 1;
+    System.out.println("apakah angka: " + angka + " ganjil? " + ganjil);
+  }
+}
