ececd52a5f4a04a24b4a746628bda49412446b6f
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-10-16 19:04:43 +0800

    Refactor menu selection logic in Day39.java

--- Day39.java
+++ Day39.java
@@ -10,35 +10,38 @@ public class Day39 {
 
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("Andi pergi ke sebuah restoran untuk makan siang. Ia melihat menu makanan yang tersedia dan memutuskan untuk memesan salah satu dari menu tersebut. Menu makanan yang tersedia adalah:\n");
+    System.out.println("Pilihan Menu: ");
     System.out.println("1. Nasi Goreng (Rp 20.000)");
     System.out.println("2. Mie Ayam (Rp 15.000)");
     System.out.println("3. Sate Kambing (Rp 30.000)");
     System.out.println("4. Es Teler (Rp 10.000)");
 
-    System.out.print("Andi memilih dengan nomor menu yang diinginkan: ");
+    System.out.print("Pilih Menu: ");
     int angka = sc.nextInt();
 
-    System.out.println(); 
-    if(angka == 1){
-      System.out.println("Anda Telah Memilih: Nasi Goreng");
-      System.out.println("Seharga Rp 20.000");
-      System.out.println("Terima kasih telah memesan!");
+    String menu = null;
+    int harga = 0;
+
+    System.out.println();
+    if (angka == 1) {
+      menu = "Nasi Goreng";
+      harga = 20000;
     } else if (angka == 2) {
-      System.out.println("Anda Telah Memilih: Mie Ayam");
-      System.out.println("Seharga Rp 15.000");
-      System.out.println("Terima kasih telah memesan!");
+      menu = "Mie Ayam";
+      harga = 15000;
     } else if (angka == 3) {
-      System.out.println("Anda Telah Memilih: Sate Kambing");
-      System.out.println("Seharga Rp 30.000");
-      System.out.println("Terima kasih telah memesan!");
+      menu = "Sate Kambing";
+      harga = 30000;
     } else if (angka == 4) {
-      System.out.println("Anda Telah Memilih: Es Teler");
-      System.out.println("Seharga Rp 10.000");
-      System.out.println("Terima kasih telah memesan!");
-    } else{
+      menu = "Es Teler";
+      harga = 10000;
+    } else {
       System.out.println("Masukan tidak ada di pilihan menu!");
+      return;
     }
-  
+
+    System.out.println("Anda Telah Memilih: " + menu);
+    System.out.println("Seharga Rp " + harga);
+    System.out.println("Terima kasih telah memesan!");
   }
 }
