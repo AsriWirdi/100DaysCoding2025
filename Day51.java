9cb4bd321416b266ceba9931cf64659dab580469
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-10-28 22:49:41 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 51 : Perulangan for

---
+++ Day51.java
@@ -0,0 +1,12 @@
+public class Day51 {
+  public static void main(String[] args) {
+    /*
+     * 100 Days Of Coding 2025
+     * Day 51 : Perulangan for
+     */
+
+    for (int i = 1; i < 5; i++) {
+      System.out.println(i);
+    }
+  }
+}
