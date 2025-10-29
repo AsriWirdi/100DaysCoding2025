f371cce137b6a2dbe4f3085fb942d1ec862a894d
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-10-29 21:52:24 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 52 : Perulangan while

---
+++ Day52.java
@@ -0,0 +1,14 @@
+public class Day52 {
+  public static void main(String[] args) {
+    /*
+     * 100 Days Of Coding 2025
+     * Day 52 : Perulangan while
+     */
+
+    int i = 1;
+    while (i <= 10) {
+      System.out.println(i);
+      i++;
+    }
+  }
+}
