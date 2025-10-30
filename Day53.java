b63bc961239c0e32b03d8218cef6c9fccbbd9252
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-10-30 20:22:43 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 53 : Keyword break

---
+++ Day53.java
@@ -0,0 +1,18 @@
+public class Day53 {
+  public static void main(String[] args) {
+    /*
+     * 100 Days Of Coding 2025
+     * Day 53 : Keyword break
+     */
+
+    int i = 1;
+    while (true) {
+      if (i > 5) {
+        break;
+      }
+      
+      System.out.println(i);
+      i++;
+    }
+  }
+}
