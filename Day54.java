356927a957d30c9c9955383de37ff30ae8ec575e
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-10-31 21:46:19 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 54 : Keyword continue

---
+++ Day54.java
@@ -0,0 +1,16 @@
+public class Day54 {
+  public static void main(String[] args) {
+    /*
+     * 100 Days Of Coding 2025
+     * Day 54 : Keyword continue
+     */
+
+    for (int i = 1; i <= 10; i++) {
+      if (i == 5) {
+        continue;
+      }
+
+      System.out.println(i);
+    }
+  }
+}
