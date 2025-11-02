8b13096c2fe4182955687a4625b76ec07011befc
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-11-02 21:46:48 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 56 : Perulangan do-while

---
+++ Day56.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Day56 {
+  public static void main(String[] args) {
+    /*
+     * 100 Days Of Coding 2025
+     * Day 56 : Perulangan do-while
+     */
+
+    Scanner sc = new Scanner(System.in);
+
+    String[] accountsUsn = { "asriwirdi", "muhrifai", "muhammadiqwal" };
+    String[] accountsPw = { "D0224325", "D0224007", "D0224025" };
+
+    String usnSaatIni = "";
+    String pwSaatIni = "";
+    int percobaan = 0;
+
+    do {
+      System.out.print("Username : ");
+      String usn = sc.next();
+
+      System.out.print("Password : ");
+      String pw = sc.next();
+
+      for (int i = 0; i < accountsPw.length; i++) {
+        usnSaatIni = accountsUsn[i];
+        pwSaatIni = accountsPw[i];
+
+        if (usnSaatIni.equalsIgnoreCase(usn) && pwSaatIni.equalsIgnoreCase(pw)) {
+          System.out.println("Login Sebagai Pengguna");
+          return;
+        } else if (usn.equalsIgnoreCase("admin") && pw.equalsIgnoreCase("admin")) {
+          System.out.println("Login Sebagai Admin");
+          return;
+        } else {
+          percobaan++;
+          continue;
+        }
+      }
+
+      System.out.println("Silahkan Coba Lagi");
+    } while (percobaan <= 3);
+    System.out.println("Login Gagal");
+  }
+}
