577a747f4f225004c482edde2828a3cb1308a7b2
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-11-05 22:53:58 +0800

    Update Day59.java
    100 Days Of Coding 2025
    Day 59 : Latihan: Mencetak angka ganjil 1-N dan angka genap 1-N

--- Day59.java
+++ Day59.java
@@ -4,7 +4,7 @@ public class Day59 {
   public static void main(String[] args) {
     /*
      * 100 Days Of Coding 2025
-     * Day 58 : Latihan: Mencetak angka 1-N dan angka N-1
+     * Day 59 : Latihan: Mencetak angka ganjil 1-N dan angka genap 1-N
      */
     Scanner sc = new Scanner(System.in);
 
