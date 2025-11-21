89fb80b6f1d19081b41d4fb8bac30740b9235774
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-11-21 23:11:42 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 75 : Method String Dasar (length(), toUpperCase(), toLowerCase()).

---
+++ Day75.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Day75 {
+  public static void main(String[] args) {
+    /*
+     * 100 Days Of Coding 2025
+     * Day 75 : Method String Dasar (length(), toUpperCase(), toLowerCase()).
+     */
+
+    String nama = "Halo Dunia";
+    System.out.println(nama.length());
+    System.out.println(nama.toUpperCase());
+    System.out.println(nama.toLowerCase());
+  }
+}
