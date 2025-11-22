0a8dc0db2c25d530165d154c16e511fa311fe14c
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-11-22 22:38:11 +0800

    Create Day76.java
    100 Days Of Coding 2025
    Day 76 : Method String untuk Pengecekan (equals(), equalIgnoreCase(), contains(), isEmpty()).

---
+++ Day76.java
@@ -0,0 +1,13 @@
+public class Day76 {
+  public static void main(String[] args) {
+    /*
+     * 100 Days Of Coding 2025
+     * Day 76 : Method String untuk Pengecekan (equals(), equalIgnoreCase(), contains(), isEmpty()).
+     */
+
+    String nama = "Asri";
+    System.out.println(nama.equals("asri"));
+    System.out.println(nama.equalsIgnoreCase("asri"));
+    System.out.println(nama.isEmpty());
+  }
+}
