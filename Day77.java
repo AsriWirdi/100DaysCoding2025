344689938827f709e022b131de79df12db78fe64
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-11-23 23:46:09 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 77 : Method String untuk Manipulasi (substring(), replace(), trim()).

---
+++ Day77.java
@@ -0,0 +1,15 @@
+public class Day77 {
+  public static void main(String[] args) {
+    /*
+     * 100 Days Of Coding 2025
+     * Day 77 : Method String untuk Manipulasi (substring(), replace(), trim()).
+     */
+
+    String nama = " Asri Wirdi ";
+    System.out.println(nama.substring(0, 5));
+
+    System.out.println(nama.replace("Asri", "Muh"));
+
+    System.out.println(nama.trim());
+  }
+}
