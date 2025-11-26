4d1fbd80e86f29a6226fd83bc01613894c2189a3
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-11-26 23:23:54 +0800

    Create Day80.java
    100 Days Of Coding 2025
    Day 80 : Array 1D

---
+++ Day80.java
@@ -0,0 +1,12 @@
+public class Day80 {
+/*
+         * 100 Days Of Coding 2025
+         * Day 80 : Array 1D
+         */
+    public static void main(String[] args) {
+        int a[] = {1, 2, 3, 4, 5};
+        for(int b : a){
+            System.out.println(b);
+        }
+    }
+}
