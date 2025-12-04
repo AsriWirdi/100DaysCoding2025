fbfc4875c985f8f11b9a45256c9fbe75d08d4c4b
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-12-04 23:23:59 +0800

    Create Day88.java
    100 Days Of Coding 2025
    Day 88 : Mengakses Elemen Array dengan Menggunakan For-each

---
+++ Day88.java
@@ -0,0 +1,13 @@
+public class Day88 {
+    public static void main(String[] args) {
+    /*
+     * 100 Days Of Coding 2025
+     * Day 88 : Mengakses Elemen Array dengan Menggunakan For-each
+     */
+        int[] arrAngka = {1, 2, 3, 4, 5};
+
+        for (int i : arrAngka) {
+            System.out.println(i);
+        }
+    }
+}
