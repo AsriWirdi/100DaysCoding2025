dc5cc922cd845347569145e3928b5f50f5b8d0ac
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-12-05 22:19:09 +0800

    Create Day89.java
    100 Days Of Coding 2025
    Day 89 : Method void

---
+++ Day89.java
@@ -0,0 +1,15 @@
+public class Day89 {
+    public void p(){
+        System.out.println("hello world");
+}
+    public static void main(String[] args) {
+        /*
+        * 100 Days Of Coding 2025
+        * Day 89 : Method void
+        */
+     
+        Day89 hw = new Day89();
+        hw.p();
+        hw.p();
+    }
+}
