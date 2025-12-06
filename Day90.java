f790b4d61460c6779ea410665cc88c4b121bc797
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-12-06 22:03:55 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 90 : Method non-void

---
+++ Day90.java
@@ -0,0 +1,15 @@
+public class Day90 {
+  public static void main(String[] args) {
+    /*
+     * 100 Days Of Coding 2025
+     * Day 90 : Method non-void
+     */
+    Day90 k = new Day90();
+
+    System.out.println(k.kali(5, 5));
+  }
+  
+  int kali(int a, int b){
+    return a * b;
+  }
+}
