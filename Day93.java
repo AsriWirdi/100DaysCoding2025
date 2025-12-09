bdbc7110bd7bc27519e53f15e369224e35709369
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-12-09 21:25:07 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 93 : Method ceil(), floor() dan round()

---
+++ Day93.java
@@ -0,0 +1,13 @@
+public class Day93 {
+  public static void main(String[] args) {
+    /*
+     * 100 Days Of Coding 2025
+     * Day 93 : Method ceil(), floor() dan round()
+     */
+
+    double a = 5.5;
+    System.out.println(Math.ceil(a));
+    System.out.println(Math.floor(a));
+    System.out.println(Math.round(a));
+  }
+}
