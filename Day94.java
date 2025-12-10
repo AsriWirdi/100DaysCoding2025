25d082542e58ffbe3b7e378c917ef9125a60e8da
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-12-10 23:24:48 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 94 : Latihan: Mencetak dengan Method Void

---
+++ Day94.java
@@ -0,0 +1,15 @@
+public class Day94 {
+  /*
+   * 100 Days Of Coding 2025
+   * Day 94 : Latihan: Mencetak dengan Method Void
+   */
+  
+  void cetakNama(String nama){
+    System.out.println("Halo nama saya " + nama);
+  }
+  public static void main(String[] args) {
+
+    Day94 n = new Day94();
+    n.cetakNama("Asri Wirdi");
+  }
+}
