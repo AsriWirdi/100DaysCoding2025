c672a67892652346861ae4a62c74b7a4e45f27ff
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2025-12-11 23:17:46 +0800

    Add files via upload
    100 Days Of Coding 2025
    Day 95 : Latihan: Mencetak dengan Parameter Void

---
+++ Day95.java
@@ -0,0 +1,15 @@
+public class Day95 {
+  /*
+   * 100 Days Of Coding 2025
+   * Day 95 : Latihan: Mencetak dengan Parameter Void
+   */
+  
+  void sapa(String nama){
+    System.out.println("Halo " + nama);
+  }
+  public static void main(String[] args) {
+
+    Day95 n = new Day95();
+    n.sapa("Asri Wirdi");
+  }
+}
